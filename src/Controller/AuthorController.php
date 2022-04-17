4956c80f02315ed088dff4968527bf9f5ba7f653
kazakovichna <kazakovichna@gmail.com>
2022-04-17 17:07:14 +1000

    Finished make function to add author now make delete function

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\AuthorRepository;
+use App\Service\AuthorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\AuthorService;
+use Symfony\Component\HttpFoundation\Request;
 
 class AuthorController extends AbstractController
 {
@@ -16,5 +17,17 @@ class AuthorController extends AbstractController
     public function getAllAuthor(AuthorService $authSer, AuthorRepository $authRep): Response
     {
         return $authSer->getAllAuthorSer($authRep);
+    }
+
+    /**
+     * @Route("/author", name="add_author", methods={"POST"})
+     * @param AuthorService $authSer
+     * @param AuthorRepository $authRep
+     * @param Request $request
+     * @return Response
+     */
+    public function add_author(AuthorService $authSer, AuthorRepository $authRep, Request $request): Response
+    {
+        return $authSer->addAuthor($authRep, $request);
     }
 }
--- src/Service/AuthorService.php
+++ src/Service/AuthorService.php
@@ -3,8 +3,9 @@
 namespace App\Service;
 
 use App\Repository\AuthorRepository;
-use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Author;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthorService extends AuthorRepository
@@ -33,6 +34,51 @@ class AuthorService extends AuthorRepository
 
         return new Response(
             json_encode($authorsMas),
+            Response::HTTP_OK,
+            ['content-type'=> 'json']
+        );
+    }
+
+    /**
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function addAuthor(AuthorRepository $authRep, Request $request): Response
+    {
+        // Получаем данные из запроса
+        $requestData = json_decode($request->getContent());
+        echo $requestData->author_name;
+        if ($requestData === null) {
+            return new Response(
+                'Empty Object Data Error 500',
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+                [['content-type'=> 'json']]
+            );
+        }
+        // Проверяем нет ли уже такого автора
+        if ($authRep->findOneBy(['author_name'=>$requestData->author_name]) !== null) {
+            return new Response(
+                'Author already exist error 500',
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+                [['content-type'=> 'json']]
+            );
+        }
+        // Валидируем данные на длинну от 1 до 255
+        if (strlen($requestData->author_name) === 0 ||
+            strlen($requestData->author_name) > 255) {
+            return new Response(
+                "Invalid data",
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+                ['content-type'=> 'json']
+            );
+        }
+        // Сохраняем данные в базу данных
+        $authorDB = new Author();
+        $authorDB->setAuthorName($requestData->author_name);
+        $authRep->add($authorDB, true);
+
+        return new Response(
+            "Author add successfully",
             Response::HTTP_OK,
             ['content-type'=> 'json']
         );
