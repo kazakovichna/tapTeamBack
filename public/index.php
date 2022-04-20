2c14879211d4dccd99673c1b4c0a1ae4157259fb
kazakovichna <kazakovichna@gmail.com>
2022-04-20 23:31:12 +1000

    apache-pack

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
@@ -12,6 +13,17 @@ return function (array $context) {
     $method = $_SERVER['REQUEST_METHOD'];
     if ($method == "OPTIONS") {
         die();
+    }
+
+    if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+        Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+    }
+
+    $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+    $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+    if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+    if($trustedProxies) {
+        Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
     }
 
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
